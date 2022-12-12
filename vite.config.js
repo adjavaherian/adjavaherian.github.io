3e8e61fe64194134c6242d4beaa553e06e3171b1
Amir Djavaherian <a_mir@icloud.com>
2022-12-12 14:51:21 -0600

    validated html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        education: resolve(__dirname, 'education.html'),
+        hobbies: resolve(__dirname, 'hobbies.html'),
+        photos: resolve(__dirname, 'photos.html'),
+        whatsnext: resolve(__dirname, 'whats-next.html'),
+      },
+    },
+  },  
 })
